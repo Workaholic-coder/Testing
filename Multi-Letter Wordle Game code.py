2707a21168abfcaa3317adfc28eba1de9bc5056a
The_Workaholic <132090746+Workaholic-coder@users.noreply.github.com>
2024-12-31 00:08:40 +0000

    -

--- Multi-Letter Wordle Game code.py
+++ Multi-Letter Wordle Game code.py
@@ -1,12 +1,15 @@
 #Multi-word Wordle Code
+x = 0
 #opens a file
-try:
-    filename = input('Enter file name: ')
-    file = open(filename, 'r')
-    file_2 = file.read()
-    file_1 = file_2.split()
-except:
-    print('Didn\'t work')
+while x == 0:
+    try:
+        filename = input('Enter file name: ')
+        file = open(filename, 'r')
+        file_2 = file.read()
+        file_1 = file_2.split()
+        x = x + 1
+    except:
+        print('Didn\'t work')
 
 list = list()
 for words in file_1:
@@ -17,3 +20,4 @@ import random
 random_word = random.choice(list)
 
 list_2 = list((random_word, random_word.upper(), random_word[0].upper() + random_word[1:], random_word.lower()))
+ 
